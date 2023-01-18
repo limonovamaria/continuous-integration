2708fc0dc5d2edc1892ea07f7f88823cd266efef
Лимонова Мария <Marusia.limonova@gmail.com>
2023-01-19 02:52:34 +0500

    r

--- Tests/PuzTest.py
+++ Tests/PuzTest.py
@@ -5,19 +5,19 @@ import pytest
 
 class PuzTest:
 
-
+    @pytest.fixture
     def test1(self):
         a = np.array([7,-1,4,13,9])
         b = np.array([13,9,7,4,-1])
         assert (ml.Puzirek(a) == b).all()
 
-
+    @pytest.fixture
     def test2(self):
         a = np.array([1, 9, -1, 5, 8])
         b = np.array([9, 8, 5, 1, -1])
         assert (ml.Puzirek(a) == b).all()
 
-
+    @pytest.fixture
     def test3(self):
         with pytest.raises(TypeError):
             ml.Puzirek(0)
