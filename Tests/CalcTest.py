6dc59fc8cc056dc110e6bb4c9644701eefe1545d
Лимонова Мария <Marusia.limonova@gmail.com>
2023-01-19 03:00:44 +0500

    t

--- Tests/CalcTest.py
+++ Tests/CalcTest.py
@@ -5,21 +5,21 @@ import pytest
 
 class CalcTest:
 
-    @pytest.fixture
+
     def test1(self):
         a = 7
         b = -3
         d = a/b
         assert (ml.Deistvie(a,"/",b) == d)
 
-    @pytest.fixture
+
     def test2(self):
         a = 7
         b = -3
         d = a+b
         assert (ml.Deistvie(a, "+", b) == d)
 
-    @pytest.fixture
+
     def test3(self):
         with pytest.raises(ZeroDivisionError):
             ml.Deistvie(7,"/",0)
--- Tests/FibTest.py
+++ Tests/FibTest.py
@@ -5,17 +5,17 @@ import pytest
 
 class FibTest:
 
-    @pytest.fixture
+
     def test1(self):
         a = np.array([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
         assert (ml.Fibonachi(10) == a).all()
 
-    @pytest.fixture
+
     def test2(self):
         a = np.array([1,1])
         assert (len(ml.Fibonachi(2)) == len(a))
 
-    @pytest.fixture
+
     def test3(self):
         with pytest.raises(TypeError):
             ml.Fibonachi(-1.2)
