38ed63e36cf05e3e2618f6d237cc3884003dcfb4
Лимонова Мария <Marusia.limonova@gmail.com>
2023-01-19 02:46:18 +0500

    e

---
+++ Tests/CalcTest.py
@@ -0,0 +1,25 @@
+from src import MyLibrary as ml
+import numpy as np
+import pytest
+
+
+class CalcTest:
+
+    #подставляем подходящие данные
+    def test1(self):
+        a = 7
+        b = -3
+        d = a/b
+        assert (ml.Deistvie(a,"/",b) == d)
+
+
+    def test2(self):
+        a = 7
+        b = -3
+        d = a+b
+        assert (ml.Deistvie(a, "+", b) == d)
+
+
+    def test3(self):
+        with pytest.raises(ZeroDivisionError):
+            ml.Deistvie(7,"/",0)
---
+++ Tests/FibTest.py
@@ -0,0 +1,23 @@
+from src import MyLibrary as ml
+import numpy as np
+import pytest
+
+
+class FibTest:
+
+
+    def test1(self):
+        a = np.array([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+        assert (ml.Fibonachi(10) == a).all()
+
+
+    def test2(self):
+        a = np.array([1,1])
+        assert (len(ml.Fibonachi(2)) == len(a))
+
+
+    def test3(self):
+        with pytest.raises(TypeError):
+            ml.Fibonachi(-1.2)
+
+
